060013aae3ce4cab87c32f16c3cc06241354cc6c
harshit08-prog <h1667651@gmail.com>
2026-05-31 06:35:29 +0000

    functions

---
+++ script7.js
@@ -0,0 +1,61 @@
+//function is used so that some particular line of code dont run at that time when we give signal only runs then
+function dance() {//function declaration
+ console.log("harshie dances")
+
+
+}
+dance();//this is the buton to tell harshie dance 
+//function is also uded to make one line of code runs multiple time
+dance();
+dance();
+dance();
+//you can make function by declaring a variable too
+let fnc = function(){//fnc is the name of function too
+console.log("heyhey")
+}//variable me save krne ko function expression khte h
+//now call the function
+fnc();
+//fat arrow function in vriable
+let fnk = ()=>{
+    console.log("fat arrow function")
+}
+fnk();
+//-------------------------------------------------------------------------------------------------
+//if you want ki har baar new thing print ho by calling same function
+//this can be done by giving a vlue in function and calling that value
+function abcd(v1) {
+    console.log(`${v1} is eating `)//backtics for saving dynamic value
+
+}
+abcd("harshit");
+abcd("sneha");
+abcd("harshie");
+function abcde(v1,v2) {//v1 v2 parameters
+    console.log(v1 + v2);
+}
+abcde(1, 2);//1 2 arguments
+function add(v1,v2) {
+    console.log(v1 + v2)//(undefined + undefined)
+}
+add();// output NaN
+//to avoid this give default value 0 to parameters
+function fng(v1 = 0, v2 = 0){
+    console.log(v1 + v2 )
+    
+}
+fng();
+// jab argument bhot sare ho toh utne hi parmeter na bnane pade islie we will use concept of rest parameters
+function bng(...val){
+    console.log(val)
+}
+bng(1,2,3,4,5,6,7,8,9,0);
+bng(2,3,4,5,4);//ek array ki form me answer ayega
+// output ---[
+//1, 2, 3, 4, 5,
+//6, 7, 8, 9, 0
+//]
+//[ 2, 3, 4, 5, 4 ] two arrays bne
+function bgh(a ,b ,c ,...val){
+    console.log(a,b,c,val)
+}
+bgh(1,2,3,4,5,6,7,8);//output---1 2 3 [ 4, 5, 6, 7, 8 ]
