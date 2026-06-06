2c6c3effa87a0b88a5d62b3369781fa2540b4ac4
harshit08-prog <h1667651@gmail.com>
2026-06-06 18:11:21 +0000

    arrays

--- script10.js
+++ script10.js
@@ -16,3 +16,21 @@ console.log(marks);
 //[3,20,30,40,50]\
 marks.splice(2,1,35,36);//2nd index se 1 value htana aur uski jagah 35 aur 36 add karna
 console.log(marks);
+let arr = [1,2,3,4,5,6];
+let newarr = arr.slice(1,5);//1st index se 5th index tk ki value nikalna
+console.log(newarr);
+arr.reverse();//reverse the main array
+let sdf = [1,2,3,4,5,6,7,8,9];
+sdf.sort(function(a,b)
+{return b - a;//descending order
+})
+console.log(sdf);
+//foreach
+sdf.forEach(function(element){
+    console.log(element+5);//sdf array ke har element me 5 add krke print krna
+});
+//MAP
+let skf = sdf.map(function(value){
+    return value*2;//sdf array ke har element ko 2 se multiply krke naya array bnana
+})
+console.log(skf);
